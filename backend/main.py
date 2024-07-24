7e18ed3075619a126ee54d86127649b711c9c7fd
Kevin Moon <skm04614@gmail.com>
2024-07-24 23:03:01 +0900

    feat: backend main 작성

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, APIRouter
 
-app = FastAPI()
+from database import Base, engine
 
 
-@app.get("/hello")
-def hello():
-    return {"message": "안녕하세요"}
+def get_app():
+    api_router = APIRouter(prefix="/api")
+
+    app = FastAPI()
+    app.include_router(api_router)
+
+    @app.on_event("startup")
+    def on_startup():
+        Base.metadata.create_all(bind=engine)
+
+    return app
+
+
+def main():
+    uvicorn.run(
+        "backend.main:get_app",
+        host="0.0.0.0",
+        port=8000,
+        reload=False,
+        factory=True
+    )
+
+
+if __name__ == "__main__":
+    main()
